5b3cd503ba84e862faa1aeaa2d6f58a3723339dc
xianhua347 <3389162133@qq.com>
2022-09-23 21:08:35 +0800

    del testing

---
+++ src/main/java/main/java/org/lykj/MyTest.java
@@ -0,0 +1,10 @@
+package main.java.org.lykj;
+
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+    void replacePhoneNumber(){
+        System.out.println(ReplacePhoneNumber.replacePhone("1111"));
+    }
+}
