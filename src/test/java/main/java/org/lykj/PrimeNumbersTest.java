ff2ddf827da6d9bc1a640e646dbcd1072e1c6d71
xianhua347 <3389162133@qq.com>
2022-09-24 17:40:13 +0800

    add PrimeNumbersTest modified PrimeNumbers

---
+++ src/test/java/main/java/org/lykj/PrimeNumbersTest.java
@@ -0,0 +1,11 @@
+package main.java.org.lykj;
+
+import org.junit.jupiter.api.Test;
+
+class PrimeNumbersTest {
+
+    @Test
+    void primeNumbersSum() {
+        PrimeNumbers.primeNumbersSum();
+    }
+}
