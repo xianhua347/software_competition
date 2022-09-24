5b9c6df5967fb753544e0d625c5e3875170dfe92
xianhua347 <3389162133@qq.com>
2022-09-24 20:36:25 +0800

    add Login and LoginTest

--- src/test/java/main/java/org/lykj/LoginTest.java
+++ src/test/java/main/java/org/lykj/LoginTest.java
@@ -1,9 +1,5 @@
 package main.java.org.lykj;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class LoginTest {
     public static void main(String[] args) {
         Login.loginWithUserPassWord();
