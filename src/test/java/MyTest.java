07ce63f5b7c20a9352ae1024f00de3fceb3ed45c
xianhua347 <3389162133@qq.com>
2022-09-24 15:53:43 +0800

    change gradle dependencies add test folder

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,9 @@
+import main.java.org.lykj.ReplacePhoneNumber;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+    void replacePhoneNumber(){
+        System.out.println(ReplacePhoneNumber.replacePhoneWithRegular("13053954937"));
+    }
+}
