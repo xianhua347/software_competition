d27f7e26d7a3fcc6ea44bbe7c1b624cdea63abc1
xianhua347 <3389162133@qq.com>
2022-09-24 18:24:16 +0800

    add 0-100素数算法笔记

--- src/main/java/main/java/org/lykj/PrimeNumbers.java
+++ src/main/java/main/java/org/lykj/PrimeNumbers.java
@@ -9,10 +9,10 @@ public class PrimeNumbers {
             for (int j = 2; j <= i;j++){
                 //遍历2-i的数字
                 if (i % j == 0 && i != j) break; //// 如果i % j余数为0，且i、j不相等，跳出if(i%j == 0 && i!=j) 说明这个数不是素数
-                if (j == i){
-                    // 判断i是否和j相等，若i==j，就说明该数是素数。因为可以被自身整除
-                    if (count % 5 == 0)
-                        System.out.println();
+                        if (j == i){
+                            // 判断i是否和j相等，若i==j，就说明该数是素数。因为可以被自身整除
+                            if (count % 5 == 0)
+                                System.out.println();
                     System.out.print(j + " ");
                     count++;
                 }
