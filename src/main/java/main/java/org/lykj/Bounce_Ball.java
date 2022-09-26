24398779f4be45b07241a1ddc3c3bda5d9706b61
xianhua347 <3389162133@qq.com>
2022-09-26 15:04:07 +0800

    feat(src):Bounce_Ball

--- src/main/java/main/java/org/lykj/Bounce_Ball.java
+++ src/main/java/main/java/org/lykj/Bounce_Ball.java
@@ -3,17 +3,20 @@ package main.java.org.lykj;
 public class Bounce_Ball {
 
     public static void main(String[] args) {
+
+        //弹起的次数
         int n = 10;
         //小球经过的经过的距离 第一次是100，从第二次开始每次弹起的高度是前一次的两倍
         double ways = 100;
         //小球弹起的高度
         final double height = bounce(n);
-        System.out.println("该球第"+n+"次弹起的高度为："+height+"米。");
 
         //计算经过的距离，从第二次开始算
         for (int i = 2; i < n; i++) {
             ways += bounce(i) * 2;
         }
+
+        System.out.println("该球第"+n+"次弹起的高度为："+height+"米。");
         System.out.println("该球到第"+n+"次落下总共经过了："+ways+"米");
     }
 
