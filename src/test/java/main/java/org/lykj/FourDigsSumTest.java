d02c4cbb788a6b35127d57aec3692e9ca4bb9ed6
xianhua347 <3389162133@qq.com>
2022-09-26 09:14:50 +0800

    test(test):add FourDigsSum Test

---
+++ src/test/java/main/java/org/lykj/FourDigsSumTest.java
@@ -0,0 +1,11 @@
+package main.java.org.lykj;
+
+import org.junit.jupiter.api.Test;
+
+class FourDigsSumTest {
+
+    @Test
+    void sum() {
+        System.out.println(FourDigitsSum.sum(1234));
+    }
+}
